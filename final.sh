7e28c4d17a5f53dd6ad9685b70eb78a3ec0dd18b
harshvardhan1998 <harshvardhan7162@gmail.com>
2020-01-30 14:26:29 +0530

    Update final.sh

--- final.sh
+++ final.sh
@@ -20,26 +20,28 @@ ssh centos@$host3 sudo su -c /home/centos/k8s-centos/prequisite.sh root
 ssh centos@$host3 sudo su -c whoami root
 
 
-ssh centos@$host1 sudo kubeadm init --control-plane-endpoint $proxy:6443 --upload-certs --pod-network-cidr=10.244.0.0/16 |  grep -m1  certificate-key > certificate.sh
+ssh centos@$host1 sudo kubeadm init --control-plane-endpoint $proxy:6443 --upload-certs --pod-network-cidr=10.244.0.0/16 |  grep -m1  certificate-key > certificate
 ssh centos@$host1 mkdir -p $HOME/.kube
 ssh centos@$host1 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 ssh centos@$host1 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-ssh centos@$host1 kubeadm token create --print-join-command | grep -m1 kubeadm > join-cmd.sh
-ssh centos@$host1 echo -n $(cat  join-cmd.sh) >  join-cmd.sh
-ssh centos@$host1 cat certificate.sh >>  join-cmd.sh
+ssh centos@$host1 kubeadm token create --print-join-command | grep -m1 kubeadm > join-cmd
+ssh centos@$host1 echo -n $(cat  join-cmd) >  join-cmd
+ssh centos@$host1 cat /home/centos/centosk8s-HA/certificate >>  join-cmd
 
 
-scp  join-cmd.sh centos@$host2:/home/ubuntu
-ssh centos@$host2 chmod +x  join-cmd.sh
-ssh centos@$host2  sudo /home/centos/join-cmd.sh 
+
+
+scp  join-cmd centos@$host2:/home/centos
+ssh centos@$host2 chmod +x  join-cmd
+ssh centos@$host2  sudo /home/centos/join-cmd
 ssh centos@$host2 mkdir -p $HOME/.kube
 ssh centos@$host2 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 ssh centos@$host2 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 
-scp  join-cmd.sh  centos@$host3:/home/ubuntu
-ssh centos@$host3 chmod +x  join-cmd.sh
-ssh centos@$host3  sudo /home/centos/join-cmd.sh
+scp  join-cmd  centos@$host3:/home/centos
+ssh centos@$host3 chmod +x  join-cmd
+ssh centos@$host3  sudo /home/centos/join-cmd
 ssh centos@$host3 mkdir -p $HOME/.kube
 ssh centos@$host3 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 ssh centos@$host3 sudo chown $(id -u):$(id -g) $HOME/.kube/config
